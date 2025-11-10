65dd84ccf86c1a2e73a203bea16ad8686910f9cb
Velichko Alexander <sasha0992@gmail.com>
2025-11-10 20:38:38 +0400

    fix: resolve root and add

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,16 +1,36 @@
 const { NotImplementedError } = require('../lib/errors')
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js')
 
 /**
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
 class BinarySearchTree {
-  root() {}
+  constructor() {
+    this.tree = null
+  }
+  root() {
+    return this.tree
+  }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented')
+  add(data) {
+    this.tree = addNode(this.tree, data)
+
+    function addNode(node, data) {
+      if (!node) {
+        return new Node(data)
+      }
+      if (node.data === data) {
+        return node
+      }
+      if (data < node.data) {
+        node.left = addNode(node.left, data)
+      } else {
+        node.right = addNode(node.right, data)
+      }
+
+      return node
+    }
   }
 
   find(/* data */) {
